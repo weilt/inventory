83367b983a0737ffc60be06ac114ca23c9ab04a0
weilt <weilt1234@163.com>
2018-09-10 09:12:15 +0800

    eureka

---
+++ commonentity/src/main/java/com/weilt/commonentity/CommonentityApplication.java
@@ -0,0 +1,12 @@
+package com.weilt.commonentity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonentityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonentityApplication.class, args);
+    }
+}
